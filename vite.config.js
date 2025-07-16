2ae0adf9f330a2f4cc8a154c4703f4c67b54ba1e
leehoyong-90 <ghdyfl90@gmail.com>
2025-07-16 17:26:40 +0900

    initial summit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/botc/',  // ← 레포 이름과 일치해야 함!
   plugins: [react()],
 })
